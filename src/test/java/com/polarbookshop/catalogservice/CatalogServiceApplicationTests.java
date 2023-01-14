7cd97f8b18cca149dfeb8bc4440ba483ea46dc1c
dev-indb <dev@indb.fr>
2023-01-14 11:53:18 +0100

    first commit

---
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,126 @@
+package com.polarbookshop.catalogservice;
+
+import com.polarbookshop.catalogservice.domain.Book;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CatalogServiceApplicationTests {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @BeforeEach
+    public void setUp() {
+        webTestClient = webTestClient.mutate()
+                .responseTimeout(Duration.ofMillis(30000))
+                .build();
+    }
+    @Test
+    void whenGetRequestWithIdThenBookReturned() {
+        var bookIsbn = "1231231230";
+        var bookToCreate = new Book(bookIsbn, "Title", "Author", 9.90);
+        Book expectedBook = webTestClient
+                .post()
+                .uri("/books")
+                .bodyValue(bookToCreate)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Book.class).value(book -> assertThat(book).isNotNull())
+                .returnResult().getResponseBody();
+
+        webTestClient
+                .get()
+                .uri("/books/" + bookIsbn)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(Book.class).value(actualBook -> {
+                    assertThat(actualBook).isNotNull();
+                    assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+                });
+    }
+
+    @Test
+    void whenPostRequestThenBookCreated() {
+        var expectedBook = new Book("1231231231", "Title", "Author", 9.90);
+
+        webTestClient
+                .post()
+                .uri("/books")
+                .bodyValue(expectedBook)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Book.class).value(actualBook -> {
+                    assertThat(actualBook).isNotNull();
+                    assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+                });
+    }
+
+    @Test
+    void whenPutRequestThenBookUpdated() {
+        var bookIsbn = "1231231232";
+        var bookToCreate = new Book(bookIsbn, "Title", "Author", 9.90);
+        Book createdBook = webTestClient
+                .post()
+                .uri("/books")
+                .bodyValue(bookToCreate)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Book.class).value(book -> assertThat(book).isNotNull())
+                .returnResult().getResponseBody();
+        var bookToUpdate = new Book(createdBook.isbn(), createdBook.title(), createdBook.author(), 7.95);
+
+        webTestClient
+                .put()
+                .uri("/books/" + bookIsbn)
+                .bodyValue(bookToUpdate)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Book.class).value(actualBook -> {
+                    assertThat(actualBook).isNotNull();
+                    assertThat(actualBook.price()).isEqualTo(bookToUpdate.price());
+                });
+    }
+
+    @Test
+    void whenDeleteRequestThenBookDeleted() {
+        var bookIsbn = "1231231233";
+        var bookToCreate = new Book(bookIsbn, "Title", "Author", 9.90);
+
+    /*
+        ne fonctionne pas sur plateforme Linux et/ou hors IDE
+        https://stackoverflow.com/questions/47286975/timeout-on-blocking-read-for-5000-milliseconds-in-spring-webflux/48655749
+        https://stackoverflow.com/questions/62838689/getting-a-lot-of-prematurecloseexception-connection-prematurely-closed-before
+        webTestClient
+                .post()
+                .uri("/books")
+                .bodyValue(bookToCreate)
+                .exchange()
+                .expectStatus().isCreated();
+    */
+
+        webTestClient
+                .delete()
+                .uri("/books/" + bookIsbn)
+                .exchange()
+                .expectStatus().isNoContent();
+
+        webTestClient
+                .get()
+                .uri("/books/" + bookIsbn)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody(String.class).value(errorMessage ->
+                    assertThat(errorMessage).isEqualTo("The book with ISBN " + bookIsbn + " was not found.")
+                );
+    }
+
+}
